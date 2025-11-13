9e2cf7baf3f468d392fb61f737460e2b13a0e102
André Lashley <andre.lashley@gmail.com>
2025-11-13 15:33:43 -0800

    Load config variables

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(AppContext.BaseDirectory)
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+string BaseUrl = config["ResourceSpace:BaseUrl"]!;
+string User = config["ResourceSpace:User"]!;
+string PrivateKey = config["ResourceSpace:PrivateKey"]!;
+
+int MaxParallel = int.Parse(config["Upload:MaxParallel"]!);
+string InboundPath = config["Upload:InboundPath"]!;
+
+Console.WriteLine(BaseUrl);
